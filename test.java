9a99e836905103962f0d640e692f241a954e0f9e
shipsy <shipsy@shipsys-MacBook-Air-2.local>
2024-12-21 18:26:58 +0530

    changed java file

--- test.java
+++ test.java
@@ -4,5 +4,8 @@ class test {
     public static void main(String args[])
     {
         System.out.println("Hello, World");
+        for(int i=0;i<100;i++){
+            System.out.println("Hello Shipsians.");
+        }
     }
 }
